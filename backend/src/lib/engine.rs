60ad7eb7a1efa93726b35cee7c7810861b1d6995
Arif Zakuan <arifzakuanbinadnan@airasia.com>
2024-12-14 00:31:53 +0800

    DF and query API

---
+++ backend/src/lib/engine.rs
@@ -0,0 +1,18 @@
+use datafusion::arrow::{record_batch, util};
+use datafusion::prelude::*;
+
+use record_batch::RecordBatch;
+
+pub async fn query() -> datafusion::error::Result<String> {
+    let ctx = SessionContext::new();
+    ctx.register_csv("uploaded", "tmp/uploaded.csv", CsvReadOptions::new())
+        .await?;
+
+    let df = ctx.sql("SELECT * FROM uploaded").await?;
+
+    let results: Vec<RecordBatch> = df.collect().await?;
+
+    let pretty_results = util::pretty::pretty_format_batches(&results)?.to_string();
+
+    Ok(pretty_results)
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,31 @@
 #[macro_use]
 extern crate rocket;
+use crate::lib::engine;
 use csv::Reader;
 use rocket::fs::TempFile;
-use std::fs::File;
+use std::{fs::File, ptr::read};
+
+mod lib;
 
 #[get("/")]
 fn index() -> &'static str {
     "The site is live"
 }
 
+#[get("/process")]
+async fn process_csv() -> String {
+    let results = engine::query().await;
+    match results {
+        Ok(s) => s,
+        Err(s) => s.strip_backtrace(),
+    }
+}
+
 #[post("/upload", data = "<file>")]
 async fn upload_csv(mut file: TempFile<'_>) -> &'static str {
-    let filepath = "./tmp/uploaded.csv";
+    let filepath = "/tmp/uploaded.csv";
     file.persist_to(filepath).await.unwrap();
 
-    // Read the CSV file
     let mut rdr = Reader::from_path(filepath).unwrap();
     for result in rdr.records() {
         let record = result.unwrap();
@@ -26,5 +37,5 @@ async fn upload_csv(mut file: TempFile<'_>) -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, upload_csv])
+    rocket::build().mount("/", routes![index, upload_csv, process_csv])
 }
